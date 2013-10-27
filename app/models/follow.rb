a661de1f9c4695dee9623e5ae6d0d717254f34a7
Grant Van Sant <grantwenzlau@gmail.com>
2013-10-27 14:55:58 -0700

    commentable basic setup

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -4,6 +4,7 @@ class Follow < ActiveRecord::Base
 
   extend ActsAsFollower::FollowerLib
   extend ActsAsFollower::FollowScopes
+  
 
   # NOTE: Follows belong to the "followable" interface, and also to followers
   belongs_to :followable, :polymorphic => true
