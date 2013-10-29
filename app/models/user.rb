813cf87453b2516c9f8fc52e7aa0c2c6a0d4c59c
Grant Van Sant <grantwenzlau@gmail.com>
2013-10-29 10:19:42 -0700

    avatar attempts

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
 
   
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/guest.png"
+ 
   #validates_attachment_presence :avatar
   #validates_attachment_size :avatar, :less_than => 5.megabytes
   #validates_attachment_content_type :avatar, :content_type => ['image/jpeg', 'image/png']
