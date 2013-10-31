c09de01321e4321b0912b40351b76b9f0b649ffa
Mountnlabs <mountnlabs@gmail.com>
2013-10-31 23:52:52 +0100

    fix on goodreads

--- config/initializers/goodreads.rb
+++ config/initializers/goodreads.rb
@@ -1,4 +1,4 @@
-#Goodreads.configure(
- # :api_key => 'LjHpAx1qKTPgaKIgIyzw',
-  #:api_secret => 'z4DcHYoaqyrYT52HWHdgzpuRPjuf8aM2i4o0K5O8g'
-#)
+Goodreads.configure(
+  :api_key => 'LjHpAx1qKTPgaKIgIyzw',
+  :api_secret => 'z4DcHYoaqyrYT52HWHdgzpuRPjuf8aM2i4o0K5O8g'
+)
