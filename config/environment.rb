78407c609ca4677b02337ee214baea58ebf66e63
Grant Van Sant <grantwenzlau@gmail.com>
2013-11-09 08:44:38 -0800

    conf

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,15 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Booksy::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'bookloop.co',
-  :enable_starttls_auto => true
-}
+#ActionMailer::Base.smtp_settings = {
+ # :address        => 'smtp.sendgrid.net',
+ # :port           => '587',
+ # :authentication => :plain,
+ # :user_name      => ENV['SENDGRID_USERNAME'],
+ # :password       => ENV['SENDGRID_PASSWORD'],
+ # :domain         => 'bookloop.co',
+ # :enable_starttls_auto => true
+#}
 
 require 'mail'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,6 +21,6 @@ Booksy::Application.configure do
     # Expands the lines which load the assets
     config.assets.debug = true
 
-    config.action_mailer.default_url_options = { :host => 'bookloop.co' }
+    #config.action_mailer.default_url_options = { :host => 'wwww.bookloop.co' }
 
 end
