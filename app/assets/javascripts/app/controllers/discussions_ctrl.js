b996ead8bc0576c663b6e2ca1120026c62d5b9b5
Pete Hawkins <pete@phawk.co.uk>
2013-11-25 22:56:09 +0000

    Add the form for creating discussions and tie it up to ng-resource

--- app/assets/javascripts/app/controllers/discussions_ctrl.js
+++ app/assets/javascripts/app/controllers/discussions_ctrl.js
@@ -1,5 +1,15 @@
 App.controller('DiscussionsCtrl', ['$scope', 'Discussion', function($scope, Discussion) {
-    console.log("Discussion", arguments);
     var book_id = App.opts.book_id;
+
     $scope.discussions = Discussion.query({ book_id: book_id });
+    $scope.discussion = new Discussion({ book_id: book_id });
+
+    $scope.newDiscussion = function(discussion) {
+
+        discussion.$save();
+
+        // Reload the discussions view
+        $scope.discussions = Discussion.query({ book_id: book_id });
+    }
+
 }]);
