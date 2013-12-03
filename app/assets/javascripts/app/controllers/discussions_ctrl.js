22cd4b545723e3b3dc9bed9cf21fba175d24b27d
Pete Hawkins <pete@phawk.co.uk>
2013-12-03 21:13:36 +0000

    Get the rest of the comments working

--- app/assets/javascripts/app/controllers/discussions_ctrl.js
+++ app/assets/javascripts/app/controllers/discussions_ctrl.js
@@ -1,5 +1,9 @@
-App.controller('DiscussionsCtrl', ['$scope', 'Discussion', function($scope, Discussion) {
+App.controller('DiscussionsCtrl', ['$scope', '$http', 'Discussion', function($scope, $http, Discussion) {
     var book_id = App.opts.book_id;
+
+    function updateDiscussions() {
+
+    }
 
     $scope.discussions = Discussion.query({ book_id: book_id });
     $scope.discussion = new Discussion({ book_id: book_id });
@@ -12,18 +16,37 @@ App.controller('DiscussionsCtrl', ['$scope', 'Discussion', function($scope, Disc
 
         // Reload the discussions view
         $scope.discussions = Discussion.query({ book_id: book_id });
+
+        // Show the discussion
+        $scope.showDiscussion(discussion);
     };
 
     $scope.showDiscussion = function(discussion, $event) {
-        $event.stopPropagation();
+        if ($event) $event.stopPropagation();
+
         $scope.newDiscussionForm = null;
         $scope.visibleDiscussion = discussion;
     };
 
     $scope.showDiscussionForm = function($event) {
-        $event.stopPropagation();
+        if ($event) $event.stopPropagation();
+
         $scope.visibleDiscussion = null;
         $scope.newDiscussionForm = true;
+    };
+
+    $scope.postComment = function($event, discussion) {
+        var comment_url = "/api/book/"+ book_id +"/discussions/"+ discussion.id +"/commentaries";
+
+        $http.post(comment_url, JSON.stringify({ commentary: { message: $scope.newCommentText } })).
+          success(function(data, status, headers, config) {
+            $scope.discussions = Discussion.query({ book_id: book_id });
+            $scope.visibleDiscussion = Discussion.get({ book_id: book_id, id: discussion.id });
+            $scope.newCommentText = "";
+          }).
+          error(function(data, status, headers, config) {
+            alert("We had an error posting your comment, please try again");
+          });
     };
 
 }]);
