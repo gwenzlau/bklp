ea32d5dd001a3b11456fad0c42c9f1f3ff5f7335
homanp <homanp@gmail.com>
2013-12-05 21:19:29 +0100

    Added tab functionality for book show menu

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,30 @@
+// Switch between book menues
+$(document).ready( function() {
+	function switchBookTabs(selector) {
+		switch(selector) {
+			case 'book-desc':
+				$('#book-reviews, #book-suggestions, #book-discussions').hide();
+				$('#'+selector+', #currently-reading').show();
+				break;
+			case 'book-reviews':
+				$('#book-desc, #currently-reading, #book-suggestions, #book-discussions').hide();
+				$('#'+selector).show();
+				break;
+			case 'book-suggestions':
+				$('#book-desc, #currently-reading, #book-reviews, #book-discussions').hide();
+				$('#'+selector).show();
+				break;
+			case 'book-discussions':
+				$('#book-desc, #currently-reading, #book-suggestions, #book-reviews').hide();
+				$('#'+selector).show();
+				break;
+		}
+	}
+
+	$('#book-nav ul li a').click( function() {
+		var selector = $(this).attr('for');
+		switchBookTabs(selector);
+		$('#book-nav ul li a').removeClass('active');
+		$(this).addClass('active');
+	});
+});
