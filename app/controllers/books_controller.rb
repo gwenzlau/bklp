276b1589acc5ca203a209d1231b6a5f38a03d4c1
Mountnlabs <mountnlabs@gmail.com>
2013-12-06 13:05:55 +0100

    Added isbn to book model and covers to suggestions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -189,6 +189,6 @@ class BooksController < ApplicationController
   private
   
   def book_params
-    params.require(:book).permit(:title, :author, :olida, :olidb, :user_id, :status, :rec, :recommend, :order)
+    params.require(:book).permit(:title, :author, :olida, :olidb, :user_id, :status, :rec, :recommend, :order, :isbn)
   end
 end
