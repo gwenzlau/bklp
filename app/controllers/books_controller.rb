81b0537a5a0f1ba29ba842d2af90a10d31c88179
Grant Van Sant <grantwenzlau@gmail.com>
2013-12-06 08:19:45 -0800

    rm conflict

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,17 +27,12 @@ class BooksController < ApplicationController
     #Users currently reading this book
     @users_reading = Book.where(:olidb => params[:id]).where(:status => "0")
     @review = Review.where(:book_id => params[:id])
-<<<<<<< HEAD
+
     
     #This will pick up 5 random books by users who have read the current book beeing viewd
     also = Book.select(:user_id).uniq.where(:olidb => params[:id])
     @also_read = Book.where(user_id: also).where(:status => "1").limit(5).order("RANDOM()")
-=======
-  
-    #This will pick up 5 random books by users who have read the current book beeing viewd
-    also = Book.select(:user_id).uniq.where(:olidb => params[:id])
-    @also_read = Book.where(user_id: also).where(:status => "1").limit(4).order("RANDOM()")
->>>>>>> new_ui
+
   end
   
   def author
