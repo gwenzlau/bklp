ababc172af5cb194bbe6ae901fa2e291c8e0a6fa
Grant Van Sant <grantwenzlau@gmail.com>
2013-12-08 16:49:51 -0800

    p

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
   def index
-  	@activities = PublicActivity::Activity.order("created_at desc").page(params[:page]).per_page(20)
+  	@activities = PublicActivity::Activity.order("created_at desc").paginate(:page => params[:page], :per_page => 5)
   end
 end
