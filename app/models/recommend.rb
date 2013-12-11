10790070fadfe70ed73307a623cebfe25a327103
Mountnlabs <mountnlabs@gmail.com>
2013-12-11 23:23:03 +0100

    activity for recommend

--- app/models/recommend.rb
+++ app/models/recommend.rb
@@ -1,5 +1,8 @@
 class Recommend < ActiveRecord::Base
+
+  include PublicActivity::Model
+  tracked owner: ->(controller, model) { controller && controller.current_user}
+  
   belongs_to :book
   belongs_to :user
-  
 end
