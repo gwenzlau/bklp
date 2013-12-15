8af2f29c93d4b0ce025cad8a0c6ce54c4e2eacd2
Grant Van Sant <grantwenzlau@gmail.com>
2013-12-15 14:04:28 -0800

    rec-work

--- db/schema.rb
+++ db/schema.rb
@@ -73,7 +73,6 @@ ActiveRecord::Schema.define(version: 20131215120232) do
     t.boolean  "recommend",  default: false
     t.integer  "order"
     t.string   "isbn"
-    t.string   "slug"
   end
 
   add_index "books", ["user_id"], name: "index_books_on_user_id"
@@ -104,12 +103,6 @@ ActiveRecord::Schema.define(version: 20131215120232) do
   add_index "comments", ["commentable_type"], name: "index_comments_on_commentable_type"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
-  create_table "conversations", force: true do |t|
-    t.string   "subject",    default: ""
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-  end
-
   create_table "discussions", force: true do |t|
     t.text     "quote"
     t.integer  "page"
@@ -137,19 +130,6 @@ ActiveRecord::Schema.define(version: 20131215120232) do
   add_index "follows", ["followable_id", "followable_type"], name: "fk_followables"
   add_index "follows", ["follower_id", "follower_type"], name: "fk_follows"
 
-  create_table "friendly_id_slugs", force: true do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
-    t.string   "sluggable_type", limit: 50
-    t.string   "scope"
-    t.datetime "created_at"
-  end
-
-  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
-  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
-
   create_table "links", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -159,26 +139,6 @@ ActiveRecord::Schema.define(version: 20131215120232) do
     t.integer  "status"
     t.string   "note"
   end
-
-  create_table "notifications", force: true do |t|
-    t.string   "type"
-    t.text     "body"
-    t.string   "subject",              default: ""
-    t.integer  "sender_id"
-    t.string   "sender_type"
-    t.integer  "conversation_id"
-    t.boolean  "draft",                default: false
-    t.datetime "updated_at",                           null: false
-    t.datetime "created_at",                           null: false
-    t.integer  "notified_object_id"
-    t.string   "notified_object_type"
-    t.string   "notification_code"
-    t.string   "attachment"
-    t.boolean  "global",               default: false
-    t.datetime "expires"
-  end
-
-  add_index "notifications", ["conversation_id"], name: "index_notifications_on_conversation_id"
 
   create_table "rails_admin_histories", force: true do |t|
     t.text     "message"
@@ -192,20 +152,6 @@ ActiveRecord::Schema.define(version: 20131215120232) do
   end
 
   add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories"
-
-  create_table "receipts", force: true do |t|
-    t.integer  "receiver_id"
-    t.string   "receiver_type"
-    t.integer  "notification_id",                            null: false
-    t.boolean  "is_read",                    default: false
-    t.boolean  "trashed",                    default: false
-    t.boolean  "deleted",                    default: false
-    t.string   "mailbox_type",    limit: 25
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
-  end
-
-  add_index "receipts", ["notification_id"], name: "index_receipts_on_notification_id"
 
   create_table "recommends", force: true do |t|
     t.integer  "user_id"
@@ -241,7 +187,6 @@ ActiveRecord::Schema.define(version: 20131215120232) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string   "slug"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
