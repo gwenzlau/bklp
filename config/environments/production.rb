2df7cb8accfc3ae744a4ed7064b27290739481f8
Grant Van Sant <grantwenzlau@gmail.com>
2013-12-24 10:24:49 -0800

    precomp css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Booksy::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+  config.assets.precompile = ['*.js', '*.css']
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
