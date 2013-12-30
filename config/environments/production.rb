9c27e65d908522a8c67cf9ff71f0168d730e2ac4
Grant Van Sant <grantwenzlau@gmail.com>
2013-12-30 11:53:14 -0800

    rm double script

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Booksy::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
    #config.action_controller.asset_host = "http://assets.example.com"
-   #config.action_controller.asset_host = "d19t704ji8lp0d.cloudfront.net"
+   config.action_controller.asset_host = "d19t704ji8lp0d.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
