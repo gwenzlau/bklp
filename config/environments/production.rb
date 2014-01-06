31f1617b5bfae94bdfa41d91235e7944a2f26710
Grant Van Sant <grantwenzlau@gmail.com>
2014-01-06 13:11:11 -0800

    config stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,13 +11,13 @@ Booksy::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   #config.assets.precompile += ['style.css', 'app.css', 'animate.css', 'bootstrap.min.css', 'discussions.css.scss', 'typehead.css']
 
@@ -81,4 +81,6 @@ Booksy::Application.configure do
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
+
+config.assets.paths << Rails.root.join('app', 'assets')
 end
