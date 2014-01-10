c4111296d4469189cb38c4ce339e4ec90fc7fde0
Jamie Barton <jamie@codethisway.com>
2014-01-10 09:12:11 +0000

    Add check_is_participating method for message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ActiveRecord::Base
+	after_save :check_is_participating
+
 	default_scope { order ('created_at desc') }
 	
 	belongs_to :conversation, counter_cache: true, touch: true
@@ -8,5 +10,13 @@ class Message < ActiveRecord::Base
 
 	def deleteable?(message_user)
 		created_at >= 5.minutes.ago && user == message_user
+	end
+
+	protected
+
+	def check_is_participating
+		if !participant = Participant.find_by(user_id: self.user_id, conversation_id: self.conversation.id)
+			Participant.create!(user_id: message.user.id, conversation_id: self.conversation.id)
+		end
 	end
 end
