44d517465b354fbc8c7e6ecd63e378b3c44146e5
madebytrolls <ejmyrvang@gmail.com>
2014-01-11 18:05:38 +0100

    Clean: Books controller with security fixes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,80 +1,51 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:update, :start_future_read, :destroy]
+  before_action :set_api, only: [:show, :author, :finishedmodal]
+  
 	def index
     @books = Book.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @books }
-    end
   end
 
-  # GET /books/1
-  # GET /books/1.json
-  def show
-    # Start a new session
-    client = Goodreads.new(Goodreads.configuration)
-    
+  def show  
     # Get the metadata from Goodreads
-    @thebook = client.book(params[:id]) unless client.book(params[:id]).blank?
+    @thebook = @client.book(params[:id]) unless @client.book(params[:id]).blank?
     @slug = @thebook.title.parameterize
   
-    # If user logged in, get user data and initialize new instance for the option to start reading the book.
     if user_signed_in?
-      @newbook = Book.new
+      @newbook = current_user.books.build
       @mybook = Book.where(:user_id => current_user.id).where(:olidb => params[:id])
       @myrecommend = Recommend.where(:user_id => current_user.id).where(:item_id => params[:id])
-      @user = current_user
       @myreview = Review.where(:user_id => current_user.id).where(:book_id => params[:id])
+      @new_recommend = Recommend.new
     end
     
     #Users currently reading this book
     @users_reading = Book.where(:olidb => params[:id]).where(:status => "0")
     @review = Review.where(:book_id => params[:id])
-
-    @new_recommend = Recommend.new
-    #Number of users who have recommended this book
     @total_recommend = Recommend.where(:item_id => params[:id]).count
-    #this is to check if there have been any recommendations
     @recommends = Recommend.where(:item_id => params[:id])
     
-    #This will pick up 5 random books by users who have read the current book beeing viewd
+    # Selecting random books for suggestions
     also = Book.select(:user_id).uniq.where(:olidb => params[:id])
     @also_read = Book.where(user_id: also).where(:status => "1").limit(4).order("RANDOM()")
-
   end
   
   def author
-    client = Goodreads.new(Goodreads.configuration)
-    @theauthor = client.author(params[:id])
-    @authorbooks = client.search_books(@theauthor.name)
-    @user = current_user
+    @theauthor = @client.author(params[:id])
+    @authorbooks = @client.search_books(@theauthor.name)
     
     @myrecommend = Recommend.where(:user_id => current_user.id).where(:item_id => params[:id])
     @total_recommends = Recommend.where(:item_id => params[:id]).where(:item_type => "author").count
     @new_recommend = Recommend.new
 
     @users_fan = Recommend.where(:item_id => params[:id]).where(:item_type => "author").limit(9).order("RANDOM()")
-
   end
 
-
-  # GET /books/new
-  # GET /books/new.json
   def new
-    @book = Book.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @book }
-    end
   end
 
-  
-  # POST /books
-  # POST /books.json
   def create
-    @book = Book.new(book_params)
-    
+    @book = current_user.books.build(book_params)
     if @book.save
       redirect_to current_user
     else
@@ -83,9 +54,7 @@ class BooksController < ApplicationController
   end
   
   def book_readit
-    @book = Book.new(book_params)
-    #@book = Book.new(params[:book])
-    
+    @book = current_user.books.build(book_params)
     if @book.save
       redirect_to current_user
     else
@@ -93,12 +62,7 @@ class BooksController < ApplicationController
     end
   end
 
-  # PUT /books/1
-  # PUT /books/1.json
   def update
-    @book = Book.find(params[:id])
-    #@book.create_activity :book_readit
-
     if @book.update_attribute(:status, "1")
       redirect_to finished_path(:id => @book.olidb)
     else
@@ -107,7 +71,7 @@ class BooksController < ApplicationController
   end
 
   def future_list
-    @future = Book.new(book_params)
+    @future = current_user.books.build(book_params)
     @future.activity key: 'book.future_list'
     
     if @future.save
@@ -118,39 +82,16 @@ class BooksController < ApplicationController
   end
   
   def start_future_read
-    @book = Book.find(params[:id])
     @book.activity key: 'book.future_read'
-
     if @book.update_attribute(:status, "0")
       redirect_to current_user
     else
       redirect_to root_path
     end
   end
-
-    def recommend_list
-  @recommend = Book.find(params[:id])
-  @recommend.activity key: 'book.recommend_list'
-  if @recommend.update_attribute(:recommend, "true")
-      redirect_to current_user
-    else
-      redirect_to root_path
-    end
-  end
-
-  def rec_list
-  @rec = Book.find(params[:id])
-  @rec.activity key: 'book.rec_list'
-  if @rec.update_attribute(:rec, "true")
-      redirect_to current_user
-    else
-      redirect_to root_path
-    end
-  end
-
-
+  
   def past_list
-    @past = Book.new(book_params)
+    @past = current_user.books.build(book_params)
     @past.activity key: 'book.past_list'
     
     if @past.save
@@ -171,12 +112,10 @@ class BooksController < ApplicationController
     end
   end
 
-   def finishedmodal
-    client = Goodreads.new(Goodreads.configuration)
-    @thebook = client.book(params[:id]) unless client.book(params[:id]).blank?
+  def finishedmodal
+    @thebook = @client.book(params[:id]) unless @client.book(params[:id]).blank?
     @slug = @thebook.title.parameterize
   
-    # If user logged in, get user data and initialize new instance for the option to start reading the book.
     if user_signed_in?
       @newbook = Book.new
       @mybook = Book.where(:user_id => current_user.id).where(:olidb => params[:id])
@@ -185,28 +124,27 @@ class BooksController < ApplicationController
     end
     
     @review = Review.where(:book_id => params[:id])
-
     @new_recommend = Recommend.new
 
-    #This will pick up 5 random books by users who have read the current book beeing viewd
+    # Select random books for suggestions
     also = Book.select(:user_id).uniq.where(:olidb => params[:id])
     @also_read = Book.where(user_id: also).where(:status => "1").limit(4).order("RANDOM()")
-
   end
 
-  # DELETE /books/1
-  # DELETE /books/1.json
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
-
-    respond_to do |format|
-      format.html { redirect_to root_path }
-      format.json { head :no_content }
-    end
+    redirect_to root_path
   end
   
   private
+  
+  def set_book
+    @book = Book.find(params[:id])
+  end
+  
+  def set_api
+    @client = Goodreads.new(Goodreads.configuration)
+  end
   
   def book_params
     params.require(:book).permit(:title, :author, :olida, :olidb, :user_id, :status, :rec, :recommend, :order, :isbn, :slug)
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,6 @@ include PublicActivity::Model
 tracked owner: ->(controller, model) { controller && controller.current_user}
 
 belongs_to :user
+belongs_to :book
 
 end
