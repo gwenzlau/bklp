80aca38c55c634d744260ed44494a20ba268c9b9
Pete Hawkins <pete@phawk.co.uk>
2014-01-11 11:46:44 +0000

    Update production asset settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,15 +3,14 @@ Booksy::Application.configure do
 
   # Code is not reloaded between requests
   config.cache_classes = true
-  
   config.eager_load = true
-  
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.js_compressor = :uglifier
@@ -22,13 +21,13 @@ Booksy::Application.configure do
   #config.assets.precompile += ['style.css', 'app.css', 'animate.css', 'bootstrap.min.css', 'discussions.css.scss', 'typehead.css']
 
   # Generate digests for assets URLs
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
-   config.action_dispatch.x_sendfile_header = nil # for apache
+  config.action_dispatch.x_sendfile_header = nil # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
@@ -47,8 +46,8 @@ Booksy::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-   #config.action_controller.asset_host = "http://assets.example.com"
-   #config.action_controller.asset_host = "d19t704ji8lp0d.cloudfront.net"
+  # config.action_controller.asset_host = "http://assets.example.com"
+  # config.action_controller.asset_host = "d19t704ji8lp0d.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
@@ -66,7 +65,7 @@ Booksy::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-    # In production, :host should be set to the actual host of your application.
+  # In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => 'www.bookloop.co' }
 
   # Log the query plan for queries taking more than this (works
@@ -74,13 +73,11 @@ Booksy::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
-
-config.assets.paths << Rails.root.join('app', 'assets')
 end
