5de382b88b7658729add75fd3398702e379e9686
madebytrolls <ejmyrvang@gmail.com>
2014-01-25 21:59:17 +0100

    Add parameterize to other users reading a book

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,7 @@
 class Author < ActiveRecord::Base
+  include PublicActivity::Model
+  tracked owner: ->(controller, model) { controller && controller.current_user}
+  
   has_many :works, dependent: :destroy
   has_many :books, through: :works
   has_many :recommends, dependent: :destroy
