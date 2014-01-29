eb778b0ebbb870f1332d8b69f414b9344b126ef1
Grant Van Sant <grantwenzlau@gmail.com>
2014-01-29 12:35:50 -0800

    rm item_id mention

--- db/migrate/20140112005650_change_recommends_with_book_id.rb
+++ db/migrate/20140112005650_change_recommends_with_book_id.rb
@@ -1,6 +1,6 @@
 class ChangeRecommendsWithBookId < ActiveRecord::Migration
   def change
-    remove_column :books, :item_id
+    remove_column :books
     add_column :books, :book_id, :integer
   end
 end
