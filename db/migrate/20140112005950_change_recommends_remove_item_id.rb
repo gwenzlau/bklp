317e70f286418cb91de9943b9a1b7b2a5677992c
Grant Van Sant <grantwenzlau@gmail.com>
2014-01-29 14:33:49 -0800

    undo

--- db/migrate/20140112005950_change_recommends_remove_item_id.rb
+++ db/migrate/20140112005950_change_recommends_remove_item_id.rb
@@ -1,6 +1,6 @@
 class ChangeRecommendsRemoveItemId < ActiveRecord::Migration
   def change
-    remove_column :recommends, :books_id
+    remove_column :books, :books_id
     remove_column :recommends, :item_id
     add_column :recommends, :book_id, :integer
   end
