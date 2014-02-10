41441738dc5439fb8ecaa12942745fee4e6b5e56
homanp <homanp@gmail.com>
2014-02-10 20:39:22 +0100

    Fixed so that guide-modal triggers on sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    '/an/example/path'
+    '?welcome=true'
    # after_sign_in_path_for(resource)
   end
 end
