c56a3015cff97a60d71362ee0bfef9f4d5ebe8f1
Jamie Barton <jamie@codethisway.com>
2014-02-10 21:32:24 +0000

    Revert "Messages fix & compare objects instead of ID's"
    This reverts commit 899bf9eeb2e2a9b92988ff2c2c5d1a68942fe00a.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,5 +2,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Booksy::Application.config.secret_token = 'fbe4fiub08432h08345h0843ht4703gf9743gf9743bf043hf043hf3405ht34580h3408gh'
-Booksy::Application.config.secret_key_base = '048rh347rg4937fb3407fg349fg4937fg3408fh4038fh43208fh4308fhg348'
+Booksy::Application.config.secret_token = ENV['RAILS_COOKIE_SECRET']
+Booksy::Application.config.secret_key_base = ENV['RAILS_COOKIE_SECRET'] 
