6e88629324c458e7d192e167793eab5e09083ddb
Grant Van Sant <grantwenzlau@gmail.com>
2014-02-17 11:56:36 -0800

    rm postmrk

--- app/controllers/postmark_controller.rb
+++
@@ -1,7 +0,0 @@
-=begin
-class PostmarkController < Postmark::ApiClient
-
-def send
-	your_api_key = '516df9ef-26a9-40b3-8f5b-96afa4da000a'
-	client = Postmark::ApiClient.new(your_api_key, secure: true)
-end=end
